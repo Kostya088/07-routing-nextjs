c3b58f25123437cfc78f172e36bb67f71ee7d520
Kostya Polishchuk <kostya.polishchuk@gmail.com>
2026-02-17 20:32:28 +0100

    add modal

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -2,11 +2,13 @@
 
 import { fetchNoteById } from "@/lib/api";
 import css from "../../../notes/[id]/page.module.css";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
+import Modal from "@/components/Modal/Modal";
 
 export default function NotePreviewClient() {
   const params = useParams();
+  const router = useRouter();
   const id = params.id as string;
 
   const {
@@ -19,40 +21,57 @@ export default function NotePreviewClient() {
     refetchOnMount: false,
   });
 
+  const handleClose = () => {
+    router.back();
+  };
+
   if (isLoading) {
-    return <div>Loading, please wait...</div>;
+    return (
+      <Modal onClose={handleClose}>
+        <div>Loading, please wait...</div>
+      </Modal>
+    );
   }
 
   if (error || !note) {
-    return <p>Something went wrong.</p>;
+    return (
+      <Modal onClose={handleClose}>
+        <p>Something went wrong.</p>
+      </Modal>
+    );
   }
 
   const createdIso = note.createdAt;
   const updatedIso = note.updatedAt;
-  const creaeted = new Date(createdIso);
+  const created = new Date(createdIso);
   const updated = new Date(updatedIso);
 
   return (
-    <div className={css.noteContainer}>
-      <h1>{note.title}</h1>
-      <span className={css.tag}>{note.tag}</span>
-      <p>{note.content}</p>
-      <div className={css.date}>
-        <p>
-          Created at: {creaeted.getFullYear()}/
-          {String(creaeted.getMonth() + 1).padStart(2, "0")}/
-          {String(creaeted.getDate()).padStart(2, "0")}{" "}
-          {String(creaeted.getHours()).padStart(2, "0")}:
-          {String(creaeted.getMinutes()).padStart(2, "0")}
-        </p>
-        <p>
-          Last updated at: {updated.getFullYear()}/
-          {String(updated.getMonth() + 1).padStart(2, "0")}/
-          {String(updated.getDate()).padStart(2, "0")}{" "}
-          {String(updated.getHours()).padStart(2, "0")}:
-          {String(updated.getMinutes()).padStart(2, "0")}
-        </p>
+    <Modal onClose={handleClose}>
+      <div className={css.noteContainer}>
+        <h1>{note.title}</h1>
+        <span className={css.tag}>{note.tag}</span>
+        <p>{note.content}</p>
+        <div className={css.date}>
+          <p>
+            Created at: {created.getFullYear()}/
+            {String(created.getMonth() + 1).padStart(2, "0")}/
+            {String(created.getDate()).padStart(2, "0")}{" "}
+            {String(created.getHours()).padStart(2, "0")}:
+            {String(created.getMinutes()).padStart(2, "0")}
+          </p>
+          <p>
+            Last updated at: {updated.getFullYear()}/
+            {String(updated.getMonth() + 1).padStart(2, "0")}/
+            {String(updated.getDate()).padStart(2, "0")}{" "}
+            {String(updated.getHours()).padStart(2, "0")}:
+            {String(updated.getMinutes()).padStart(2, "0")}
+          </p>
+        </div>
+        <button className={css.backBtn} onClick={handleClose}>
+          Close
+        </button>
       </div>
-    </div>
+    </Modal>
   );
 }
